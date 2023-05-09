f3fa7b50c80bda2d60818e0d06aa8e0b859bf49e
jmyapple9 <jmyapple9@gmail.com>
2023-05-09 11:13:02 +0800

    fail

--- lab08_part1.py
+++ lab08_part1.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
+# GPIO.cleanup()
 PIN_BUTTON = 4
 PIN_LED = 18
+PIN_LED2 = 14
 PIN_SERVO = 23
 PIN_BUZZER = 24
 # Set up GPIO pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_BUZZER, GPIO.OUT)
 GPIO.setup(PIN_LED, GPIO.OUT)
+GPIO.setup(PIN_LED2, GPIO.OUT)
 GPIO.setup(PIN_SERVO, GPIO.OUT)
 GPIO.setup(PIN_BUTTON, GPIO.IN)
 # servo
@@ -25,13 +28,28 @@ GPIO.output(PIN_LED, GPIO.HIGH)
 time.sleep(1)
 GPIO.output(PIN_LED, GPIO.LOW)
 time.sleep(1)
+# led2
+GPIO.output(PIN_LED2, GPIO.LOW)
+time.sleep(1)
+GPIO.output(PIN_LED2, GPIO.HIGH)
+time.sleep(1)
 # buzzer
 GPIO.output(PIN_BUZZER, GPIO.HIGH)
 time.sleep(1)
 GPIO.output(PIN_BUZZER, GPIO.LOW)
 time.sleep(1)
 # button
-while True:
-    if GPIO.input(PIN_BUTTON):
-        print("pressed")
-        time.sleep(1)
+try:
+    while True:
+        if GPIO.input(PIN_BUTTON):
+            print("pressed")
+            
+            time.sleep(1)
+except KeyboardInterrupt:
+    # print("STOP")
+    None
+except:  
+    # print("Other error or exception occurred!" )
+    None
+finally:  
+    GPIO.cleanup()
